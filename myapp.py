3bd4202dd7a2e09ece1e4a8732db0c897bcf36da
Jasmine <jahzbhrowne@gmail.com>
2019-07-18 17:12:19 +0100

    more corrections

--- myapp.py
+++ myapp.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-import os
-import psycopg2
+#import os
+#import psycopg2
 
-DATABASE_URL = os.environ['DATABASE_URL']
+#DATABASE_URL = os.environ['DATABASE_URL']
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+#conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 
 app = Flask(__name__)
@@ -16,7 +16,7 @@ def index():
 if __name__== '__main__':
     app.run(debug=True, host='0.0.0.0')
     
-@app.route('/submit_survey')
+#@app.route('/submit_survey')
 
 
 
