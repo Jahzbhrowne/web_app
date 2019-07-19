1af85a6ae4c7ce80693bca402942d5f09df89621
Jasmine <jahzbhrowne@gmail.com>
2019-07-19 12:56:22 +0100

    m_additions

--- myapp.py
+++ myapp.py
@@ -13,6 +13,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+    
+@app.route('/thanks')
+def index():
+    return render_template('thanks.html')
 
 if __name__== '__main__':
     app.run(debug=True, host='0.0.0.0')
