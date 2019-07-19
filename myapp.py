d4fb1b3e14303b19d3424e4c90000d7cc87e3f4c
Jasmine <jahzbhrowne@gmail.com>
2019-07-19 12:37:41 +0100

    additions

--- myapp.py
+++ myapp.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
+"""
 #import os
 #import psycopg2
 
 #DATABASE_URL = os.environ['DATABASE_URL']
 
 #conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
+"""
 
 app = Flask(__name__)
 
